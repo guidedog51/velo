59c7ddc044c340de1aaa069f7f4ce4a70e26f20b
guidedog51 <nordl2@pacbell.net>
2018-01-29 09:34:54 -0800

    add IOrder interface

--- BikeDistributor.Test/OrderTest.cs
+++ BikeDistributor.Test/OrderTest.cs
@@ -1,4 +1,5 @@
-﻿using BikeDistributor.Models;
+﻿using BikeDistributor.Interfaces;
+using BikeDistributor.Models;
 using BikeDistributor.Utilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -7,27 +8,27 @@ namespace BikeDistributor.Test
     [TestClass]
     public class OrderTest
     {
-        private readonly static Bike Defy = new Bike { Brand = "Giant", Model = "Defy 1", Price = 1000.00M };   // ("Giant", "Defy 1", 1000);
-        private readonly static Bike Elite = new Bike { Brand = "Specialized", Model = "Venge Elite", Price = 2010.00M };   //   //("Specialized", "Venge Elite", 2010);
-        private readonly static Bike DuraAce = new Bike { Brand = "Specialized", Model = "S-Works Venge Dura-Ace", Price = 5555.55M };   //("Specialized", "S-Works Venge Dura-Ace", 5555);
-        private readonly static Bike Tour = new Bike { Brand = "Gazelle", Model = "Tour Populair", Price = 1000.00M };
-        private readonly static Order _order = new Order(new Company { Name = "Anywhere Bike Shop" });                                                                                                                                 //var bike = new Bike { Brand = "Gazelle", Model = "Tour Populair", Price = 1000 };
+        private readonly static IBike Defy = new Bike { Brand = "Giant", Model = "Defy 1", Price = 1000.00M };   // ("Giant", "Defy 1", 1000);
+        private readonly static IBike Elite = new Bike { Brand = "Specialized", Model = "Venge Elite", Price = 2010.00M };   //   //("Specialized", "Venge Elite", 2010);
+        private readonly static IBike DuraAce = new Bike { Brand = "Specialized", Model = "S-Works Venge Dura-Ace", Price = 5555.55M };   //("Specialized", "S-Works Venge Dura-Ace", 5555);
+        private readonly static IBike Tour = new Bike { Brand = "Gazelle", Model = "Tour Populair", Price = 1000.00M };
+        private readonly static IOrder _order = new Order(new Company { Name = "Anywhere Bike Shop" });                                                                                                                                 //var bike = new Bike { Brand = "Gazelle", Model = "Tour Populair", Price = 1000 };
+        private readonly static OrderUtilities _utilities = new OrderUtilities();
 
         [TestMethod]
         public void ReceiptOneDefy()
         {
             //order.AddLine(new Line(Defy, 1));
-            Assert.AreEqual(ResultStatementOneDefy, _order.ReceiptText());
+            Assert.AreEqual(ResultStatementOneDefy, _order.ReceiptText);
         }
 
 
         [TestMethod]
         public void GetSalesTaxRate()
         {
-            var util = new OrderUtilities();
             var address = new CompanyAddress { State = "California", County = "Alameda" };
 
-            var taxRate = util.GetSalesTaxRate(address);
+            var taxRate = _utilities.GetSalesTaxRate(address);
 
             Assert.IsNotNull(taxRate);
             Assert.AreEqual(9.5M, taxRate);
@@ -36,11 +37,8 @@ namespace BikeDistributor.Test
         [TestMethod]
         public void GetVolumeDiscount()
         {
-            var util = new OrderUtilities();
-            //var bike = new Bike { Brand = "Gazelle", Model = "Tour Populair", Price = 1000 };
-
             var lineItem = new LineItem { Bike = Defy, Quantity = 30 };
-            var discount = util.GetVolumeDiscount(lineItem);
+            var discount = _utilities.GetVolumeDiscount(lineItem);
 
             Assert.IsNotNull(discount);
             Assert.AreEqual(.9M, discount);
@@ -57,9 +55,8 @@ Total: $1,072.50";
         [TestMethod]
         public void ReceiptOneElite()
         {
-            var order = new Order(new Company { Name = "Anywhere Bike Shop" });
             //order.AddLine(new Line(Elite, 1));
-            Assert.AreEqual(ResultStatementOneElite, order.ReceiptText());
+            Assert.AreEqual(ResultStatementOneElite, _order.ReceiptText);
         }
 
         private const string ResultStatementOneElite = @"Order Receipt for Anywhere Bike Shop
@@ -72,7 +69,7 @@ Total: $2,145.00";
         public void ReceiptOneDuraAce()
         {
             //order.AddLine(new Line(DuraAce, 1));
-            Assert.AreEqual(ResultStatementOneDuraAce, _order.ReceiptText());
+            Assert.AreEqual(ResultStatementOneDuraAce, _order.ReceiptText);
         }
 
         private const string ResultStatementOneDuraAce = @"Order Receipt for Anywhere Bike Shop
@@ -85,7 +82,7 @@ Total: $5,362.50";
         public void HtmlReceiptOneDefy()
         {
             //order.AddLine(new Line(Defy, 1));
-            Assert.AreEqual(HtmlResultStatementOneDefy, _order.ReceiptHtml());
+            Assert.AreEqual(HtmlResultStatementOneDefy, _order.ReceiptHtml);
         }
 
         private const string HtmlResultStatementOneDefy = @"<html><body><h1>Order Receipt for Anywhere Bike Shop</h1><ul><li>1 x Giant Defy 1 = $1,000.00</li></ul><h3>Sub-Total: $1,000.00</h3><h3>Tax: $72.50</h3><h2>Total: $1,072.50</h2></body></html>";
@@ -94,7 +91,7 @@ Total: $5,362.50";
         public void HtmlReceiptOneElite()
         {
             //order.AddLine(new Line(Elite, 1));
-            Assert.AreEqual(HtmlResultStatementOneElite, _order.ReceiptHtml());
+            Assert.AreEqual(HtmlResultStatementOneElite, _order.ReceiptHtml);
         }
 
         private const string HtmlResultStatementOneElite = @"<html><body><h1>Order Receipt for Anywhere Bike Shop</h1><ul><li>1 x Specialized Venge Elite = $2,000.00</li></ul><h3>Sub-Total: $2,000.00</h3><h3>Tax: $145.00</h3><h2>Total: $2,145.00</h2></body></html>";
@@ -103,7 +100,7 @@ Total: $5,362.50";
         public void HtmlReceiptOneDuraAce()
         {
            // order.AddLine(new Line(DuraAce, 1));
-            Assert.AreEqual(HtmlResultStatementOneDuraAce, _order.ReceiptHtml());
+            Assert.AreEqual(HtmlResultStatementOneDuraAce, _order.ReceiptHtml);
         }
 
         private const string HtmlResultStatementOneDuraAce = @"<html><body><h1>Order Receipt for Anywhere Bike Shop</h1><ul><li>1 x Specialized S-Works Venge Dura-Ace = $5,000.00</li></ul><h3>Sub-Total: $5,000.00</h3><h3>Tax: $362.50</h3><h2>Total: $5,362.50</h2></body></html>";    
